3c0edad1e3fbf8b149568dc4337fc3c6f90d1f54
hu22333 <605610127@qq.com>
2019-04-29 15:19:06 +0800

    第一修改

--- python learn/python学习网站（廖雪峰）/python基础/数据类型和变量.py
+++ python learn/python学习网站（廖雪峰）/python基础/数据类型和变量.py
@@ -1,24 +1,24 @@
 print(1+1)
 print(0xff00)
 print(1.23e9)
-#转义字符串
+# 转义字符串
 print("I'm OK")
 print('I\'m \"OK\"!')
-#布尔
-print(3>2)
-print(3>5)
-print(3>2 or 3>5)
-print(3>2 and 3>5)
-#变量
-a=1 #1为整数
+# 布尔
+print(3 > 2)
+print(3 > 5)
+print(3 > 2 or 3 > 5)
+print(3 > 2 and 3 > 5)
+# 变量
+a = 1  # 1为整数
 print(a)
-a='abc' #abc为字符串
+a = 'abc'  # abc为字符串
 print('a')
-x=10
-x=x+2
+x = 10
+x = x+2
 print(x)
-#运算
+# 运算
 print(10/3)
 print(9/3)
 print(10//3)
-print(10%3)
+print(10 % 3)
---
+++ python learn/python学习网站（廖雪峰）/python基础/条件判断.py
@@ -0,0 +1,9 @@
+age = 25
+if age >= 18:  # 注意有：号
+    print('hu is', age)  # 注意要缩进
+    print('adult')
+elif age <= 6:
+    print('hu is', age)
+    print('teenager')
+else:
+    print('kid')
